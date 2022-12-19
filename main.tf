ef11cd7560dbeb2e9bec93c968ad409e04a67001
hari9876 <haribaskar.v@gmail.com>
2022-12-19 15:06:26 +0000

    commit3

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "azurerm_resource_group" "harirg" {
-  # name     ="${terraform.workspace}-grp"
-  name     ="harirg"
+  name     ="${terraform.workspace}-grp"
+  # name     ="harirg"
   location = local.location  
 }
 
 resource "azurerm_virtual_network" "network" {
   name                = local.virtual_network.name
   location            = local.location  
-  resource_group_name = azurerm_resource_group.harirg
+  resource_group_name = azurerm_resource_group.harirg.name
   address_space       = [local.virtual_network.address_space]
   depends_on = [
     azurerm_resource_group.harirg
@@ -17,7 +17,7 @@ resource "azurerm_virtual_network" "network" {
 
 resource "azurerm_subnet" "subnet" {    
     name                 = "SubnetA"
-    resource_group_name  = azurerm_resource_group.harirg
+    resource_group_name  = azurerm_resource_group.harirg.name
     virtual_network_name = local.virtual_network.name
     address_prefixes     = [cidrsubnet(local.virtual_network.address_space,8,0)]
     depends_on = [
